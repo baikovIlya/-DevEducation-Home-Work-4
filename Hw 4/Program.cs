15bbb96396ad406bc2c4094034d2e7318429e7a1
baikovIlya <baikusha23@gmail.com>
2022-03-03 23:56:46 +0300

    HW1 done

--- Hw 4/Program.cs
+++ Hw 4/Program.cs
@@ -1,2 +1,10 @@
 ﻿using Hw_4;
-Variables.Equation();
+int task2a = Convert.ToInt32(Console.ReadLine());
+int task2b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Variables.Equation(task2a, task2b));
+
+int task4a = Convert.ToInt32(Console.ReadLine());
+int task4b = Convert.ToInt32(Console.ReadLine());
+int task4c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Variables.SearchX(task4a, task4b, task4c));
+
--- Hw 4/variables.cs
+++ Hw 4/variables.cs
@@ -8,9 +8,18 @@ namespace Hw_4
 {
     static class Variables
     {
-        public static void Equation()
+        public static double Equation(int a,int b)
         {
-            Console.WriteLine("equation");
+            double result = (5 * a + b * b) / (b - a);
+            return result;
+        }
+
+        //Пользователь вводит 3 не равных 0 числа (A, B и С). Выведите в консоль решение
+        //(значение X) линейного уравнения стандартного вида, где A*X+B=C.
+        public static double SearchX(int a,int b,int c)
+        {
+            double x = (double)(c - b) / a;
+            return x;
         }
     }
 }
