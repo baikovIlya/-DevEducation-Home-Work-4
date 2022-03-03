e984c2db0fb3d306c60b26c6e356d4a8d65c1aa6
baikovIlya <baikusha23@gmail.com>
2022-03-04 00:02:58 +0300

    HW1 done v2

--- Hw 4/Program.cs
+++ Hw 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Hw_4;
+
 int task2a = Convert.ToInt32(Console.ReadLine());
 int task2b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(Variables.Equation(task2a, task2b));
@@ -8,3 +9,9 @@ int task4b = Convert.ToInt32(Console.ReadLine());
 int task4c = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(Variables.SearchX(task4a, task4b, task4c));
 
+Console.WriteLine("Pyataya");
+int task5x1 = Convert.ToInt32(Console.ReadLine());
+int task5y1 = Convert.ToInt32(Console.ReadLine());
+int task5x2 = Convert.ToInt32(Console.ReadLine());
+int task5y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Variables.TwoPoints(task5x1, task5y1, task5x2, task5y2));
--- Hw 4/variables.cs
+++ Hw 4/variables.cs
@@ -21,5 +21,16 @@ namespace Hw_4
             double x = (double)(c - b) / a;
             return x;
         }
+
+        //Пользователь вводит 4 числа (X1, Y1, X2, Y2), описывающие координаты 2-х точек
+        //на координатной плоскости. Выведите уравнение прямой в формате Y=AX+B,
+        //проходящей через эти точки.
+        public static string TwoPoints(int x1,int y1, int x2,int y2)
+        {
+            double a = (y1 - y2) / (x1 - x2);
+            double b = y2 - a * x2;
+            string result = $"Y={a}X+{b}";
+            return result;
+        }
     }
 }
